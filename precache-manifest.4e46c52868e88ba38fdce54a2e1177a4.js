7620de4b72667f370086d856de00ddb273dd721d
Momchil Georgiev <fristys@gmail.com>
2019-08-16 19:07:06 +0200

    Updates

---
+++ precache-manifest.4e46c52868e88ba38fdce54a2e1177a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3a6675fd196d0c14a367707423e051",
+    "url": "/scoutbase-code-challenge/index.html"
+  },
+  {
+    "revision": "b73feefc83c9362b333f",
+    "url": "/scoutbase-code-challenge/static/css/main.645dca33.chunk.css"
+  },
+  {
+    "revision": "2420831542929bf34001",
+    "url": "/scoutbase-code-challenge/static/js/2.4d5a556b.chunk.js"
+  },
+  {
+    "revision": "b73feefc83c9362b333f",
+    "url": "/scoutbase-code-challenge/static/js/main.fc8db386.chunk.js"
+  },
+  {
+    "revision": "99534b75c3ee1371d1e0",
+    "url": "/scoutbase-code-challenge/static/js/runtime~main.882672c0.js"
+  }
+]);
